bc08949abf4695135facfdb56d961d264354d328
DGorohhov <gordaniil23@gmail.com>
2022-07-18 15:46:53 +0300

    first commit 2

---
+++ src/main/java/eu/gordaniil/rikexercisebe/RikExerciseBeApplication.java
@@ -0,0 +1,13 @@
+package eu.gordaniil.rikexercisebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikExerciseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikExerciseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/gordaniil/rikexercisebe/RikExerciseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.gordaniil.rikexercisebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RikExerciseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
